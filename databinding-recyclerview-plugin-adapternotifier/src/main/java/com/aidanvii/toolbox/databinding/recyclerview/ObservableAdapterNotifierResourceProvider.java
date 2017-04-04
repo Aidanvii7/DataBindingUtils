eef3e08ce6b370ea546118a333e73e205897a26e
Aidan McWilliams <aidan.vii@googlemail.com>
2017-04-04 17:54:14 +0200

    Added a composite interface and class that combines a NotifiableObservable and AdapterNotifierResourceProvider

---
+++ databinding-recyclerview-plugin-adapternotifier/src/main/java/com/aidanvii/toolbox/databinding/recyclerview/ObservableAdapterNotifierResourceProvider.java
@@ -0,0 +1,9 @@
+package com.aidanvii.toolbox.databinding.recyclerview;
+
+import com.aidanvii.toolbox.databinding.NotifiableObservable;
+/**
+ * Created by aidan.vii@gmail.com on 04/04/17.
+ */
+
+public interface ObservableAdapterNotifierResourceProvider extends NotifiableObservable, AdapterNotifierResourceProvider {
+}
